3c7d1a2edf2264ab7389fb7ce9e0f70f63803221
phamminh0811 <1phamminh0811@gmail.com>
2022-10-24 10:32:05 +0700

    Add exercise for workshop 2

---
+++ exercise_workshop_2/target/debug/deps/exercise_workshop_2-9bb69c73209bb39e.d
@@ -0,0 +1,5 @@
+/home/anhminh/Rust/Exercise/exercise_workshop_2/target/debug/deps/exercise_workshop_2-9bb69c73209bb39e.rmeta: src/main.rs
+
+/home/anhminh/Rust/Exercise/exercise_workshop_2/target/debug/deps/exercise_workshop_2-9bb69c73209bb39e.d: src/main.rs
+
+src/main.rs:
---
+++ exercise_workshop_2/target/debug/deps/exercise_workshop_2-f845bc4518c064bc.d
@@ -0,0 +1,5 @@
+/home/anhminh/Rust/Exercise/exercise_workshop_2/target/debug/deps/exercise_workshop_2-f845bc4518c064bc.rmeta: src/main.rs
+
+/home/anhminh/Rust/Exercise/exercise_workshop_2/target/debug/deps/exercise_workshop_2-f845bc4518c064bc.d: src/main.rs
+
+src/main.rs:
